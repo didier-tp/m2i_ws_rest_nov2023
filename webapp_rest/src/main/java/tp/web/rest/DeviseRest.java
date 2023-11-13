53787534b22bc42c0405d345fd9407a84f079850
didier-tp <d>
2023-11-13 13:27:31 +0100

    init

--- webapp_rest/src/main/java/tp/web/rest/DeviseRest.java
+++ webapp_rest/src/main/java/tp/web/rest/DeviseRest.java
@@ -1,8 +1,21 @@
 package tp.web.rest;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import tp.web.dto.Devise;
 
 @Path("my-api/devise")
+@Produces("application/json")
 public class DeviseRest {
+	
+	@GET
+	@Path("/{code}")
+	//http://localhost:8080/webapp_rest/rest/my-api/devise/EUR
+	public Devise getDevise(@PathParam("code") String code) {
+	return new Devise(1L,code,"...",1.123);
+	}
 
 }
---
+++ webapp_rest/src/main/java/tp/web/rest/MyWSRestConfig.java
@@ -0,0 +1,9 @@
+package tp.web.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class MyWSRestConfig extends Application {
+
+}
