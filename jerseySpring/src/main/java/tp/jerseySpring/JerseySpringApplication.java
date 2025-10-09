4e12947d00156362bab8afb81bca1e2dd37a0663
didier Defrance <didier@d-defrance.fr>
2025-10-09 11:12:59 +0200

    jerseySpring genere par spring initializr

---
+++ jerseySpring/src/main/java/tp/jerseySpring/JerseySpringApplication.java
@@ -0,0 +1,13 @@
+package tp.jerseySpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerseySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerseySpringApplication.class, args);
+	}
+
+}
---
+++ jerseySpring/src/test/java/tp/jerseySpring/JerseySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package tp.jerseySpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerseySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
